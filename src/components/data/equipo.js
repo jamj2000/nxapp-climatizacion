c884b361e640b98a62867876ef5f1fbbd135926f
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-08-11 19:47:16 +0200

    fix bug

--- src/components/data/equipo.js
+++ src/components/data/equipo.js
@@ -32,34 +32,35 @@ export default async function DataEquipo({ id, operacion }) {
             where: { id },
             include: { proyecto: true }
         })
-    // Proyectos que pertenecen al usuario con userId
-    // proyectos = await readProyectos ({ userId: userId })
-    proyectos = await prisma.proyecto.findMany({
-        select: {
-            id: true,
-            nombre: true,
-        },
-        where: { userId: userId }
-    })
+        // Proyectos que pertenecen al usuario con userId
+        // proyectos = await readProyectos ({ userId: userId })
+        proyectos = await prisma.proyecto.findMany({
+            select: {
+                id: true,
+                nombre: true,
+            },
+            where: { userId: userId }
+        })
 
 
-    switch (operacion) {
-        case CRUD.CREATE: texto = "Crear Equipo"; action = createEquipo; break;
-        case CRUD.READ: texto = "Volver"; action = noAction; disabled = true; break;
-        case CRUD.UPDATE: texto = "Actualizar Equipo"; action = updateEquipo; break;
-        case CRUD.DELETE: texto = "Eliminar Equipo"; action = deleteEquipo; disabled = true; break;
-        default:
+        switch (operacion) {
+            case CRUD.CREATE: texto = "Crear Equipo"; action = createEquipo; break;
+            case CRUD.READ: texto = "Volver"; action = noAction; disabled = true; break;
+            case CRUD.UPDATE: texto = "Actualizar Equipo"; action = updateEquipo; break;
+            case CRUD.DELETE: texto = "Eliminar Equipo"; action = deleteEquipo; disabled = true; break;
+            default:
+        }
+
+        return (
+            <FormEquipo
+                texto={texto}
+                action={action}
+                equipo={equipo}
+                proyectos={proyectos}
+                disabled={disabled}
+            >
+            </FormEquipo>
+        )
     }
-
-    return (
-        <FormEquipo
-            texto={texto}
-            action={action}
-            equipo={equipo}
-            proyectos={proyectos}
-            disabled={disabled}
-        >
-        </FormEquipo>
-    )
 }
 
