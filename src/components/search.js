a85b0d295f2191c79a585d75c6166868fb55f2f2
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-09-10 15:21:37 +0200

    remove toString from search

--- src/components/search.js
+++ src/components/search.js
@@ -3,35 +3,35 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useDebouncedCallback } from 'use-debounce';
 
 function Search() {
-  const searchParams = useSearchParams()
-  const pathname = usePathname();
-  const { replace } = useRouter();
-
-  
-  const handleSearch = useDebouncedCallback((text) => {
-    const params = new URLSearchParams(searchParams);
-    if (text) {
-      params.set('query', text);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }, 300)
+    const searchParams = useSearchParams()
+    const pathname = usePathname();
+    const { replace } = useRouter();
 
 
-  return (
-    <div className="text-right">
-    <input
-      type='search'
-      className={`text-black p-2 pl-10 rounded-full my-5 
-      bg-[url('/lupa.svg')] bg-[length:16px_16px] bg-[center_left_10px] bg-no-repeat focus:outline-blue-300`}
-      placeholder={'Buscar por nombre...'}
-      onChange={(e) => handleSearch(e.target.value) }
-      defaultValue={searchParams.get('query')?.toString()}
-    />
-  </div>
+    const handleSearch = useDebouncedCallback((text) => {
+        const params = new URLSearchParams(searchParams);
+        if (text)
+            params.set('query', text);
+        else
+            params.delete('query');
 
-  )
+        replace(`${pathname}?${params}`);
+    }, 300)
+
+
+    return (
+        <div className="text-right">
+            <input
+                type='search'
+                className={`text-black p-2 pl-10 rounded-full my-5 
+                  bg-[url('/lupa.svg')] bg-[length:16px_16px] bg-[center_left_10px] bg-no-repeat focus:outline-blue-300`}
+                placeholder={'Buscar por nombre...'}
+                onChange={(e) => handleSearch(e.target.value)}
+                defaultValue={ searchParams.get('query') }
+            />
+        </div>
+
+    )
 }
 
 export default Search
