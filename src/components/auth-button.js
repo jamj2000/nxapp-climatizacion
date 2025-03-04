48290a0d66304e6179197a3a5a13c85366ebe9fe
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-03-04 14:33:36 +0100

    auth-button

--- src/components/auth-button.js
+++ src/components/auth-button.js
@@ -5,29 +5,29 @@ import { auth } from '@/auth'
 async function AuthButton() {
   const sesion = await auth()
 
-  {
-    sesion
-      ?
-      <Link href="/auth/logout"
-        className="px-4 py-2 flex gap-2 items-center rounded-lg bg-white/80 dark:bg-blue-400/80 transition duration-500 hover:bg-white hover:shadow-xl">
-        <img
-          src={sesion.user?.image ?? "/images/user.svg"}
-          className="w-6 rounded-[50%] bg-white outline outline-1 outline-white"
-          alt="FloWeather Logo"
-        />
-        Logout
-      </Link>
-      :
-      <Link href="/auth/login"
-        className="px-4 py-2 flex gap-2 items-center rounded-lg bg-white/80 dark:bg-blue-400/80  transition duration-500 hover:bg-white hover:shadow-xl">
-        <img
-          src={"/images/logo-login.png"}
-          className="w-6"
-          alt="FloWeather Logo"
-        />
-        Login
-      </Link>
-  }
+
+  if (sesion) return (
+    <Link href="/auth/logout"
+      className="px-4 py-2 flex gap-2 items-center rounded-lg bg-white/80 dark:bg-blue-400/80 transition duration-500 hover:bg-white hover:shadow-xl">
+      <img
+        src={sesion.user?.image ?? "/images/user.svg"}
+        className="w-6 rounded-[50%] bg-white outline outline-1 outline-white"
+        alt="FloWeather Logo"
+      />
+      Logout
+    </Link>)
+
+  return (
+    <Link href="/auth/login"
+      className="px-4 py-2 flex gap-2 items-center rounded-lg bg-white/80 dark:bg-blue-400/80  transition duration-500 hover:bg-white hover:shadow-xl">
+      <img
+        src={"/images/logo-login.png"}
+        className="w-6"
+        alt="FloWeather Logo"
+      />
+      Login
+    </Link>
+  )
 }
 
 export default AuthButton;
